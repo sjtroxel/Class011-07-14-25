0953a41dd06bd506c586d4d9c88538139b6bfee8
sjtroxel <sjtroxel@protonmail.com>
2025-07-11 06:13:53 -0500

    my token is decoded and verified from JWT.io :)

--- authentication_and_serialization_tutorial/app/controllers/sessions_controller.rb
+++ authentication_and_serialization_tutorial/app/controllers/sessions_controller.rb
@@ -2,9 +2,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password])
-      render json: { token: '123' }
+      token = jwt_encode(user_id: user.id)
+      render json: { token: token }, status: :ok
     else
-      render json: { error: 'Invalid username or password' }, status: :unauthorized
+      render json: { error: 'Unauthorized' }, status: :unauthorized
     end
   end
+    
+  private 
+  
+    def jwt_encode(payload, exp = 24.hours.from_now)
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.credentials.secret_key_base)
+   end
 end
